1dbbcf0af7b9c04cede360be232b7bde81e64799
Name <daniil.adamov.2001@gmail.com>
2024-02-17 11:43:09 -0600

    Finished

--- Tudus/ContentView.swift
+++ Tudus/ContentView.swift
@@ -13,57 +13,84 @@ struct ContentView: View {
     @State private var userInput = ""
     @State private var items: [Item] = []
     
-    let columns = [GridItem(.adaptive(minimum: 100))]
-//    let columns = [GridItem(.fixed(150))]
-    
+    var columns: [GridItem] {
+        [GridItem(.flexible(minimum: 100, maximum: .infinity))]
+    }
     
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.cyan, .blue]),
                            startPoint: .bottomLeading,
                            endPoint: .topTrailing)
-            .ignoresSafeArea(.all)
+                .ignoresSafeArea(.all)
             VStack {
-                TextField("Enter item", text: $userInput)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(10)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.blue, lineWidth: 1)
-                    )
-                    .shadow(radius: 3)
-                    .padding(.init(top: 30, leading: 50, bottom: 15, trailing: 50))
-                    .font(.body)
-                    .multilineTextAlignment(.center)
-                
-                Button("Add Item") {
-                    addItem()
+                Image("cat")
+                    .resizable()
+                    .scaledToFit()
+                    .aspectRatio(1, contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .padding(.top, 10)
+                VStack {
+                    TextField("Wine", text: $userInput)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.blue, lineWidth: 1)
+                        )
+                        .shadow(radius: 3)
+                        .padding(.init(top: 30, leading: 50, bottom: 5, trailing: 50))
+                        .font(.body)
+                        .multilineTextAlignment(.center)
+                        .autocorrectionDisabled()
+                    
+                    Button(action: {
+                        addItem()
+                    }) {
+                        HStack {
+                            Text("Add Item")
+//                                .font(.subheadline)
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding(12)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .cornerRadius(10)
+                        .shadow(radius: 3)
+                    }
+                    .padding(.init(top: 5, leading: 50, bottom: 15, trailing: 50))
                 }
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .font(.headline)
-                .cornerRadius(10)
-                .shadow(radius: 5)
                 
                 ScrollView {
                     LazyVGrid(columns: columns, spacing: 10) {
                         ForEach(items, id: \.self) { item in
                             Button(action: {
                                 deleteItem(item)
-                            }) {
+                            }) { HStack {
                                 Text(item.todo)
                                     .lineLimit(nil)
                                     .padding()
-                                    .frame(maxWidth: .infinity, minHeight: 50)
-                                    .background(Color.white)
-                                    .cornerRadius(10)
+                                    .foregroundStyle(.black)
                             }
-                            .padding(5)
+                            
+                            .frame(maxWidth: .infinity, minHeight: 50, maxHeight: 50)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.blue, lineWidth: 1)
+                            )
+                            .shadow(radius: 3)
+                            }
+                            .padding(.init(top: 0, leading: 50, bottom: 0, trailing: 50))
                         }
                     }
+                    .scrollIndicators(.hidden)
+                    .animation(.default, value: items)
                 }
+                
             }
         }
     }
